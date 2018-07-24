28628dce061d4c563d4b6567fa7d3efcee5edfab
Felipe Reis <falreis@gmail.com>
2018-07-23 21:44:47 -0300

    Folder structure changes and add some files

---
+++ code/data_loader-bsds.py
@@ -0,0 +1,60 @@
+## usage (to train set)
+#python bsds_data_loader.py --set=train
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+import cv2
+import numpy as np
+import itertools
+
+from helper import *
+import os
+import argparse
+import glob
+
+DataPath = './datasets/BSDS500/'
+height = 320
+width = 480
+data_shape = height*width
+n_classes = 2
+
+reduced_image_size = (width, height, 3)
+
+def load_data(mode):
+    data = []
+    label = []
+
+    path = DataPath + mode + "/*."
+    pathg = DataPath + mode + "_ground/*."
+
+    images = glob.glob(path + "jpg") + glob.glob(path + "png")
+    images.sort()
+
+    grounds = glob.glob(pathg + "jpg") + glob.glob(pathg + "png")
+    grounds.sort()
+
+    for image, ground in zip(images, grounds):
+        reduced_image = cv2.resize(cv2.imread(image), dsize=reduced_image_size[:2], interpolation=cv2.INTER_CUBIC)
+        reduced_ground = cv2.resize(cv2.imread(ground), dsize=reduced_image_size[:2], interpolation=cv2.INTER_CUBIC)
+
+        data.append(np.rollaxis(normalized(reduced_image), 2))
+        label.append(one_hot_bsds(reduced_ground, height = height, width = width, classes = n_classes))
+    return np.array(data), np.array(label)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--set", type = str)
+args = parser.parse_args()
+set_name = args.set
+
+print(set_name)
+
+data, label = load_data(set_name)
+len_data = len(data)
+label = np.reshape(label,(len_data,data_shape, n_classes))
+
+np.save(("data/BSDS500/" + set_name + "_data"), data)
+np.save(("data/BSDS500/" + set_name + "_label"), label)
+
+print('Done')
---
+++ code/model-kitti_unet.py
@@ -0,0 +1,64 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import os
+
+os.environ['KERAS_BACKEND'] = 'theano'
+#os.environ['THEANO_FLAGS']='mode=FAST_RUN,device=cuda,floatX=float32,optimizer=None'
+
+import keras.models as models
+from keras.models import Model
+from keras.layers.core import Layer, Dense, Dropout, Activation, Flatten, Reshape, Permute
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, UpSampling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers import concatenate, Input
+
+from keras import backend as K
+K.set_image_data_format('channels_first')
+
+import numpy as np
+import json
+np.random.seed(7) # 0bserver07 for reproducibility
+
+height = 184
+width = 616
+data_shape = height*width
+classes = 2
+
+inputs = Input((classes, height, width))
+
+conv1 = Convolution2D(32, (3, 3), activation='relu', padding='same')(inputs)
+conv1 = Dropout(0.2)(conv1)
+conv1 = Convolution2D(32, (3, 3), activation='relu', padding='same')(conv1)
+pool1 = MaxPooling2D(pool_size=(2, 2), data_format="channels_first")(conv1)
+
+conv2 = Convolution2D(64, (3, 3), activation='relu', padding='same')(pool1)
+conv2 = Dropout(0.2)(conv2)
+conv2 = Convolution2D(64, (3, 3), activation='relu', padding='same')(conv2)
+pool2 = MaxPooling2D(pool_size=(2, 2), data_format="channels_first")(conv2)
+
+conv3 = Convolution2D(128, (3, 3), activation='relu', padding='same')(pool2)
+conv3 = Dropout(0.2)(conv3)
+conv3 = Convolution2D(128, (3, 3), activation='relu', padding='same')(conv3)
+
+up1 = concatenate([UpSampling2D(size=(2, 2))(conv3), conv2], axis=1)
+conv4 = Convolution2D(64, (3, 3), activation='relu', padding='same')(up1)
+conv4 = Dropout(0.2)(conv4)
+conv4 = Convolution2D(64, (3, 3), activation='relu', padding='same')(conv4)
+
+up2 = concatenate([UpSampling2D(size=(2, 2))(conv4), conv1], axis=1)
+conv5 = Convolution2D(32, (3, 3), activation='relu', padding='same')(up2)
+conv5 = Dropout(0.2)(conv5)
+conv5 = Convolution2D(32, (3, 3), activation='relu', padding='same')(conv5)
+
+conv6 = Convolution2D(classes, (1, 1), activation='relu',padding='same')(conv5)
+conv6 = Reshape((classes, data_shape), input_shape=(classes, height, width))(conv6)
+conv6 = Permute((2, 1))(conv6)
+
+conv7 = Activation('softmax')(conv6)
+
+#model = Model(input=inputs, output=conv7)
+model = Model(inputs=inputs, outputs=conv7)
+
+# Save model to JSON
+with open('uNet_kitti_model.json', 'w') as outfile:
+    outfile.write(json.dumps(json.loads(model.to_json()), indent=2))
