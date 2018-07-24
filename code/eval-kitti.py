15c5ea84a3106a261c66ece1fa45350a9f4df61e
Felipe Reis <falreis@gmail.com>
2018-07-23 21:58:04 -0300

    Change structure, rename files and update code

--- code/eval-kitti.py
+++ code/eval-kitti.py
@@ -21,9 +21,17 @@ import visualize as vis
 from skimage import io
 import glob
 from skimage import data
+import argparse
 
 np.random.seed(7)
 
+#parser
+parser = argparse.ArgumentParser()
+parser.add_argument("--net", type = str)
+args = parser.parse_args()
+net_parse = args.net
+
+#shape
 height = 184 #~375/2
 width = 616 #~1242/2
 data_shape = height*width
@@ -32,15 +40,24 @@ classes = 2
 original_height = 375
 original_width = 1242
 
+# define parameters
+json_model, weights_file = "", ""
+if net_parse == "unet":
+    json_model = 'uNet_kitti_model.json'
+    weights_file = "segnet_kitti_weights.best.hdf5"
+else:
+    json_model = 'segNet_kitti_model.json'
+    weights_file = "unet_kitti_weights.best.hdf5"
+
 # load the model:
-with open('segNet_kitti_model.json') as model_file:
-    segnet_basic = models.model_from_json(model_file.read())
+with open(json_model) as model_file:
+    net_basic = models.model_from_json(model_file.read())
 
 # load weights
-segnet_basic.load_weights("kitti_weights.best.hdf5")
+net_basic.load_weights(weights_file)
 
 # Compile model (required to make predictions)
-segnet_basic.compile(loss="categorical_crossentropy", optimizer='adadelta', metrics=["accuracy"])
+net_basic.compile(loss="categorical_crossentropy", optimizer='adadelta', metrics=["accuracy"])
 
 batch_size = 1
 
@@ -68,7 +85,7 @@ for dataset in datasets:
         original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
 
         #predict
-        output = segnet_basic.predict_proba(test_image[np.newaxis, :])
+        output = net_basic.predict_proba(test_image[np.newaxis, :])
         pred_image = vis.visualize(np.argmax(output[0],axis=1).reshape((height,width)), label_colours, False)
         
         #expand predict to the size of the original image
