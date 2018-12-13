c4e94e5c186523b8fa1ee84b086d8cd432622555
Felipe Reis <falreis@gmail.com>
2018-12-13 12:53:18 -0200

    Update code - remove cpu as default

--- code/AMRP/kitti.py
+++ code/AMRP/kitti.py
@@ -1,7 +1,7 @@
 
 import os
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+#os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
