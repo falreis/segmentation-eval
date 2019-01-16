2dae5798b3de0cc02d0bd5c3199b9f03cc98f312
Felipe Reis <falreis@gmail.com>
2019-01-16 10:23:40 -0200

    Update paper based on overleaf changes

--- code/kitti/show_model.py
+++ code/kitti/show_model.py
@@ -7,12 +7,14 @@ import model_kitti as mk
 from keras.utils import plot_model
 
 #K.set_image_data_format('channels_first')
+'''
 print('########### VGG16 ###########')
 model = VGG16(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
 #print(model.summary())
 plot_model(model, to_file='_vgg16.png')
+'''
 
 print('########### ALO ###########')
 model2 = mk.model_alo(merge_name='avg', side_output=False)
-#print(model2.summary())
-plot_model(model2, to_file='_alo.png')
+print(model2.summary())
+#plot_model(model2, to_file='_alo.png')
